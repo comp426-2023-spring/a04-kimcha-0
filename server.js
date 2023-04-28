fcb9b640be76fb93c0bcbf5b1e302455e82a911d
David <davidcha@email.unc.edu>
2023-04-28 12:27:03 -0400

    first attempt

--- server.js
+++ server.js
@@ -39,8 +39,27 @@ app.get('/app/rps/play/', (req, res) => {
   res.send(rps(req.body.shot));
 });
 
+app.get('/app/rps/play/:shot/', (req, res) => {
+  res.status(200);
+  res.send(rps(req.params.shot));
+});
+
 app.get('/app/rpsls/play/', (req, res) => {
   res.status(200);
   res.send(rpsls(req.query.shot));
 });
 
+app.get('/app/rpsls/play/', (req, res) => {
+  res.status(200);
+  res.send(rpsls(req.body.shot));
+});
+
+app.get('/app/rpsls/play/:shot/', (req, res) => {
+  res.status(200);
+  res.send(rpsls(req.params.shot));
+});
+
+app.get("*", (req, res) => {
+  res.status(404);
+  res.send("404 NOT FOUND");
+});
