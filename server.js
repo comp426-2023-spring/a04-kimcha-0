8e8bfdac27a32268f6ed10676c7687b7bef3bdbf
David <davidcha@email.unc.edu>
2023-04-28 00:22:30 -0400

    import express and handle server port

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 import { rps, rpsls } from "../a03-kimcha-0/lib/rpsls.js";
 
+let express = require('express');
+
+let app = express();
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log('Listening on port ${port}'));
 
