a6789c48409a293075474f721222f86e32bee557
David <davidcha@email.unc.edu>
2023-04-28 10:18:04 -0400

    added shebang

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { rps, rpsls } from "../a03-kimcha-0/lib/rpsls.js";
 import minimist from 'minimist';
 import express from 'express';
@@ -8,4 +9,3 @@ const port = args.port || 5000;
 const app = express();
 
 app.listen(port, () => console.log('Listening on port ${port}'));
-
