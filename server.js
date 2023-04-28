1d0a9c31dd280b66be4db797ee367bc82b3214ae
David <davidcha@email.unc.edu>
2023-04-28 12:38:41 -0400

    typos

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const args = minimist(process.argv.slice(2));
 const port = args.port || 5000;
 const app = express();
 
-app.use(express.urelencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.listen(port, () => console.log('Listening on port ${port}'));
 
